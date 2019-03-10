5e1ad3791f83e7272a9f00fd492e5b4b1e7a6890
Adam Crawford <adamcrawfordaf@yahoo.com>
2019-03-10 16:11:27 -0600

    new commit

---
+++ Labs/Objective7Lab1.java
@@ -0,0 +1,14 @@
+ public class Objective7Lab1 {
+   public static void main(String[] args) {
+     int Doug = 0;
+     do {
+       printName(Doug);
+     } while (Doug != 0);
+
+
+     }
+
+   public static void printName(int Doug) {
+     System.out.println("Hello Doug");
+ }
+}
